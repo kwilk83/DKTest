221ed00a63573b6996f61806efe183c2b355843d
Kevin Wilkinson <Kevin@Christines-MacBook-Air.local>
2019-09-27 17:32:03 -0400

    abstracting search methodology slightly, correction to return type  of searchMulti method

--- DKTest/SwingScanner.m
+++ DKTest/SwingScanner.m
@@ -8,87 +8,39 @@
 
 #import "SwingScanner.h"
 #import "SwingData.h"
+#import <CoreGraphics/CoreGraphics.h>
 
 @implementation SwingScanner
 
 -(int)searchContinuityAboveValue:(NSArray *)data indexBegin:(int)indexBegin indexEnd:(int)indexEnd threshold:(float)threshold winLength:(int)winLength {
     
-    int continuityCounter = 0;
-    
-    for (int i = indexBegin; i <= indexEnd; i++)  {
-        
-        float value = [data[i] floatValue];
-        
-        if (value > threshold) {
-            continuityCounter++;
-        } else {
-            continuityCounter = 0;
-        }
-        
-        if (continuityCounter >= winLength) {
-            return i - continuityCounter;
-        }
-    }
-    return -1;
+    return [self searchData:data data2:nil indexBegin:indexBegin indexEnd:indexEnd threshold1:threshold threshold2:CGFLOAT_MAX winLength:winLength];
 }
 
 -(int)backSearchContinuityWithinRange:(NSArray *)data indexBegin:(int)indexBegin indexEnd:(int)indexEnd thresholdLo:(float)thresholdLo thresholdHi:(float)thresholdHi winLength:(int)winLength {
     
-    int continuityCounter = 0;
-    
-    for (int i = indexBegin; i >= indexEnd; i--)  {
-        
-        float value = [data[i] floatValue];
-        
-        if (value > thresholdLo && value < thresholdHi) {
-            continuityCounter++;
-        } else {
-            continuityCounter = 0;
-        }
-        
-        if (continuityCounter >= winLength) {
-            return i;
-        }
-    }
-    return -1;
+    NSArray* reveresedArray = [[data reverseObjectEnumerator] allObjects];
+    return [self searchData:reveresedArray data2:nil indexBegin:indexBegin indexEnd:indexEnd threshold1:thresholdLo threshold2:thresholdHi winLength:winLength];
 }
 
 -(int)searchContinuityAboveValueTwoSignals:(NSArray *)data1 data2:(NSArray *)data2 indexBegin:(int)indexBegin indexEnd:(int)indexEnd threshold1:(float)threshold1 threshold2:(float)threshold2 winLength:(int)winLength {
     
-    int continuityCounter = 0;
-    
-    for (int i = indexBegin; i <= indexEnd; i++)   {
-        
-        float value1 = [data1[i] floatValue];
-        float value2 = [data2[i] floatValue];
-        
-        if (value1 > threshold1 && value2 > threshold2) {
-            continuityCounter++;
-        } else {
-            continuityCounter = 0;
-        }
-        
-        if (continuityCounter >= winLength) {
-            return i;
-        }
-    }
-    
-    return -1;
+    return [self searchData:data1 data2:data2 indexBegin:indexBegin indexEnd:indexEnd threshold1:threshold1 threshold2:threshold2 winLength:winLength];
 }
 
 -(NSArray*)searchMultiContinuityWithinRange:(NSArray *)data indexBegin:(int)indexBegin indexEnd:(int)indexEnd thresholdLo:(float)thresholdLo thresholdHi:(float)thresholdHi winLength:(int)winLength {
     
     NSMutableArray* returnArray = [NSMutableArray new];
     NSMutableArray* mutArr = [NSMutableArray new];
-    int continuityCounter = 0;
+    long continuityCounter = 0;
     
-    for (int i = indexBegin; i <= indexEnd; i++)  {
+    for (long i = indexBegin; i <= indexEnd; i++)  {
         
         float value = [data[i] floatValue];
         
         if (value > thresholdLo && value < thresholdHi) {
             continuityCounter++;
-            [mutArr addObject:[NSString stringWithFormat:@"%i", i - continuityCounter]];
+            [mutArr addObject:[NSIndexPath indexPathWithIndexes:(NSUInteger*)i - continuityCounter length:continuityCounter]];
         } else if (continuityCounter >= winLength) {
             [returnArray addObject:mutArr];
             mutArr = [NSMutableArray new];
@@ -101,4 +53,38 @@
     return returnArray;
 }
 
+
+-(int)searchData:(NSArray*)data1 data2:(NSArray*)data2 indexBegin:(int)indexBegin indexEnd:(int)indexEnd threshold1:(float)threshold1 threshold2:(float)threshold2 winLength:(int)winLength {
+    int continuityCounter = 0;
+    
+    for (int i = indexBegin; i <= indexEnd; i++)  {
+        
+        if (data2) {
+
+            float value1 = [data1[i] floatValue];
+            float value2 = [data2[i] floatValue];
+            
+            if (value1 > threshold1 && value2 > threshold2) {
+                continuityCounter++;
+            } else {
+                continuityCounter = 0;
+            }
+            
+        } else {
+            
+            float value = [data1[i] floatValue];
+            if (value > threshold1 && value < threshold2) {
+                continuityCounter++;
+            } else {
+                continuityCounter = 0;
+            }
+        }
+        
+        
+        if (continuityCounter >= winLength) {
+            return i - continuityCounter;
+        }
+    }
+    return -1;
+}
 @end
